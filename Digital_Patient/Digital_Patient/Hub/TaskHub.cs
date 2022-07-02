dc926fb738d7efd380198ef6cab2ec1f9d8a454a
Tomasz Krzemiński <koral2323@gmail.com>
2022-07-02 09:27:00 +0200

    Blazor with signalR

---
+++ Digital_Patient/Digital_Patient/Hub/TaskHub.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using Syncfusion.Blazor.Charts.Chart.Internal;
+
+namespace Digital_Patient.Hubs
+{
+    public class TaskHub : Hub
+    {
+
+
+        public async Task Update(string UserId, int TaskId)
+        {
+
+            await Clients.User(UserId).SendAsync("UpdateTask", TaskId);
+
+        }
+
+
+    }
+}
